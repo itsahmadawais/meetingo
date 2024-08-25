00a5642b1141be37b0b77d4f74d9ba360a746e4c
Awais Ahmad <awaisahmad1276@gmail.com>
2024-08-25 06:22:52 +0500

    Removes unused Imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
-import { Features } from "./_components/Features";
 export default function Home() {
   return (
     <div>
