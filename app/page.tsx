1fe4b035a5d4750224f08764d7cecfc843027f40
Awais Ahmad <awaisahmad1276@gmail.com>
2024-08-25 06:21:45 +0500

    Fixed HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
+import Image from "next/image";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
-
+import { Features } from "./_components/Features";
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <Hero/>
-      <div className='w-2/3 mx-auto'>
-        <div className="bg-primary h-32 rounded-lg p-10 text-center">
-          <h2 className="font-bold text-5xl text-white">Super Charge Your Meetings!</h2>
-        </div>
-      </div>
+      <Header />
+      <Hero />
     </div>
   );
 }
